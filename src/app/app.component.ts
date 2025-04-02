eea2b743659f8851aecd0973378b1a8d619db0f9
SalunkeTejas2002 <salunketejas2019@gmail.com>
2025-04-02 12:56:32 +0530

    Initial commit - Student Registration Form

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+title = 'student-registration-app';
+
+student = {name:'',age: null,email:'',course:''};
+students: any[] = [];
+courses = ['Computer Science', 'Mechanical', 'Civil', 'Electrical', 'Electronics'];
+
+registerStudent() {
+if(this.student.name && this.student.age && this.student.email && this.student.course){
+  this.students.push({ ...this.student});
+  this.resetForm();
+}
+}
+
+deleteStudent(index: number) {
+  this.students.splice(index, 1);
+}
+
+resetForm() {
+  this.student = { name: '', age: null, email: '', course: '' };
+}
+
+}
