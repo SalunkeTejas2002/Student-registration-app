a7a4c369dd949b2a59bdf742edca24e05be5f22b
SalunkeTejas2002 <salunketejas2019@gmail.com>
2025-04-04 12:50:06 +0530

    Updated grade color logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
 title = 'student-registration-app';
 
-student = {name:'',age: null,email:'',course:''};
+student = {name:'',age: null,email:'',course:'', grade:null};
 students: any[] = [];
 courses = ['Computer Science', 'Mechanical', 'Civil', 'Electrical', 'Electronics'];
 
 registerStudent() {
-if(this.student.name && this.student.age && this.student.email && this.student.course){
+if(this.student.name && this.student.age && this.student.email && this.student.course && this.student.grade){
   this.students.push({ ...this.student});
   this.resetForm();
 }
@@ -24,7 +24,20 @@ deleteStudent(index: number) {
 }
 
 resetForm() {
-  this.student = { name: '', age: null, email: '', course: '' };
+  this.student = { name: '', age: null, email: '', course: '', grade:null };
+}
+
+getGradeClass(grade: number): string {
+  if (grade >= 0 && grade <= 39) {
+    return 'red-bg';
+  } else if (grade >= 40 && grade <= 60) {
+    return 'yellow-bg';
+  } else if (grade >= 61 && grade <= 80) {
+    return 'blue-bg';
+  } else if (grade >= 81 && grade <= 100) {
+    return 'green-bg';
+  }
+  return '';
 }
 
 }
